ae0afc3c136442857795b34deacb66ef513278cb
Beytullah ZOR <beytullahzor7@gmail.com>
2022-10-07 16:25:53 +0300

    Updated2

--- stock-service/src/main/java/com/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/stockservice/kafka/OrderConsumer.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderConsumer {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
 
     @KafkaListener(topics = "${spring.kafka.topic.name}",
             groupId = "${spring.kafka.consumer.group-id}")
